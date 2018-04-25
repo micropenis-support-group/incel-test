dd7cd9ce8dcc0a5ae02efb9ede9c00e383c7ab3e
Dan Leonard <daniel@danleonard.us>
2018-04-25 01:46:16 -0500

    Started js

---
+++ dot-mover.js
@@ -0,0 +1,9 @@
+function prepare() {
+    var S = document.getElementById("chart");
+    var chart = S.getSVGDocument();
+    var dot = chart.getElementById('pinpoint');
+}
+
+function moveX(xMove) {
+    dot.setAttribute()
+}
