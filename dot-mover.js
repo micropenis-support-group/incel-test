496cc96b34c8c81cf29b68d6f9c8d5a266ab9a52
Dan Leonard <daniel@danleonard.us>
2018-04-25 01:59:35 -0500

    Got it debuggable

--- dot-mover.js
+++ dot-mover.js
@@ -4,6 +4,12 @@ function prepare() {
     var dot = chart.getElementById('pinpoint');
 }
 
-function moveX(xMove) {
-    dot.setAttribute()
+function moveDot(xOffset, yOffset) {
+    var S = document.getElementById("chart");
+    var chart = S.getSVGDocument();
+    var dot = chart.getElementById('pinpoint');
+    if (dot) {
+        var transformAttr = ' translate(' + xOffset + ',' + yOffset + ')';
+        dot.setAttribute('transform', transformAttr);
+    }
 }
