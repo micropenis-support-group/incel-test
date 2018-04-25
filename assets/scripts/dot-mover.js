295d1da02ffa8a24981ccfb5a24c89986a35a350
Dan Leonard <daniel@danleonard.us>
2018-04-25 18:19:48 -0500

    lol

--- assets/scripts/dot-mover.js
+++ assets/scripts/dot-mover.js
@@ -1,13 +1,10 @@
 function prepare() {
-    var S = document.getElementById("chart");
-    var chart = S.getSVGDocument();
-    var dot = chart.getElementById('pinpoint');
 }
 
 function moveDot(xOffset, yOffset) {
-    var S = document.getElementById("chart");
-    var chart = S.getSVGDocument();
-    var dot = chart.getElementById('pinpoint');
+    var chart = document.getElementById("chart").getSVGDocument();
+    var dot = chart.getElementById("pinpoint");
+
     if (dot) {
         var transformAttr = ' translate(' + xOffset + ',' + yOffset + ')';
         dot.setAttribute('transform', transformAttr);
