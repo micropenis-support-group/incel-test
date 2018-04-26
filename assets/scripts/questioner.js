195d1a6a8fa7f6fa3e1361a73aa52135a89513df
Dan Leonard <daniel@danleonard.us>
2018-04-25 22:15:17 -0500

    Some stuff that works and some that doesn't

--- assets/scripts/questioner.js
+++ assets/scripts/questioner.js
@@ -1,21 +1,45 @@
 ﻿var questionList;
+var numAnswers;
+var isLooks;
+
+function logAnswer(answerId) {
+    
+}
 
 function loadQuestion(questionId) {
+    // Load the question
     var q = questionList[questionId];
-    console.log("Question list collected.");
+    console.log("Question loaded into memory.");
+
+    // Display prompt
     $("#quizAreaPrompt").prop('innerHTML', q.prompt);
     console.log("Prompt " + questionId + ", \"" + q.prompt + "\" displayed.");
 
     // Create a button for each answer
     $.each(q.answers, function (index, value) {
         $("<button>", {
-            "class": "btn btn-outline-primary btn-lg btn-block",
+            "class": "answer btn btn-outline-primary btn-lg btn-block",
             "text": value,
             "id": "quizA" + index
         }).appendTo("#quizAreaAnswers");
+        numAnswers = index;
     });
     console.log("Question list " + questionId + " displayed.");
+
+    // Retrieve looks or personality
+    isLooks = q.looks;
+    if (isLooks) {
+        console.log("Question " + questionId + " is about looks.");
+    } else {
+        console.log("Question " + questionId + " is about personality.");
+    }
 }
+
+$("button.answer").click(function () {
+    console.log("Click detected.");
+    $(this).class = "answer btn btn-success btn-lg btn-block";
+    logAnswer($(this).id.substring(5));
+});
 
 $.getJSON("assets/data/questions.json",
     function(data) {
