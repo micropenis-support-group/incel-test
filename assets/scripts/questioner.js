b762a9f84ad8d9ec3cd2bb857037715eadd9b6f6
Dan Leonard <daniel@danleonard.us>
2018-04-25 20:56:48 -0500

    Load questions from json

--- assets/scripts/questioner.js
+++ assets/scripts/questioner.js
@@ -1,1 +1,27 @@
-﻿
+﻿var questionList;
+
+$.getJSON("assets/data/questions.json",
+    function(data) {
+        console.log(data);
+        questionList = data.questions;
+    });
+
+$(document).ready(function () {
+    console.log("Document loaded.");
+    loadQuestion(0);
+});
+
+//var my_JSON_object = JSON.parse(request("../data/questions.json"));
+
+function loadQuestion(questionId) {
+    var q = questionList[questionId];
+    $("quizPrompt").html = q.prompt;
+
+    // Create a button for each answer
+    $.each(q.answers, function (index, value) {
+        $("<button>", {
+            "text": value,
+            "id": "quizA" + index
+        }).appendTo("#answers");
+    });
+}
